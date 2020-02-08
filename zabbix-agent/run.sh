5918428a7d369d367986713fbff3c3d81c4505b5
tslpre <registrations@stefanoprenna.com>
2020-02-08 15:22:29 +0100

    Update run.sh

--- zabbix-agent/run.sh
+++ zabbix-agent/run.sh
@@ -15,9 +15,9 @@ echo "#!/bin/ash" > /opt/permissions
 echo "chmod 660 /dev/vchiq" >> /etc/zabbix/permissions
 echo "chown root:video /dev/vchiq" >> /etc/zabbix/permissions
 
-exec chmod a+rx /etc/zabbix/permissions
+#exec chmod a+rx /etc/zabbix/permissions
 
-exec /etc/zabbix/permissions
+#exec /etc/zabbix/permissions
 
 echo "
 Server=$SERVER
